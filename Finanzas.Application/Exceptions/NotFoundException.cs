1df4acb25e424af2ac9c845c8b21902e109b4a32
Rey Halsall <reduhq25@gmail.com>
2025-11-09 21:18:45 -0600

    Mejorando manejo de errores

--- Finanzas.Application/Exceptions/NotFoundException.cs
+++ Finanzas.Application/Exceptions/NotFoundException.cs
@@ -1,11 +1,26 @@
-﻿
+﻿using System;
+
 namespace Finanzas.Application.Exceptions
 {
     public class NotFoundException : ApplicationException
     {
-        public NotFoundException(string name, object key) 
-            : base($"Entity \"{name}\" ({key}) no fue encontrado")
+        public string EntityName { get; }
+
+        public object Key { get; }
+
+        public int ErrorCode { get; } = 404;
+        public NotFoundException(string entityName, object key)
+            : base($"No se encontró la entidad \"{entityName}\" con identificador ({key}).")
         {
+            EntityName = entityName;
+            Key = key;
+        }
+
+        public NotFoundException(string message, string? entityName = null, object? key = null)
+            : base(message)
+        {
+            EntityName = entityName ?? "Desconocida";
+            Key = key ?? "N/A";
         }
     }
 }
